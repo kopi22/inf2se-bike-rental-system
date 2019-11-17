93d360984792b75bcba2cada7cdf78ba19b7cc92
Linda <linda.mazanova@gmail.com>
2019-11-17 15:50:47 +0000

    New branch with impl.

---
+++ src/uk/ac/ed/bikerental/CompanyAValuationPolicy.java
@@ -0,0 +1,14 @@
+package uk.ac.ed.bikerental;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class CompanyAValuationPolicy implements ValuationPolicy {
+
+	@Override
+	public BigDecimal calculateValue(Bike bike, LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
