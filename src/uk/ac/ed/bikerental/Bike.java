128c49ddcec321d9fb3da1b1cf9e7fca7798da99
kopi22 <kpijanowski99@gmail.com>
2019-11-23 14:07:20 +0000

    Made fields private and added function getReplacementValue

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -7,33 +7,37 @@ import java.util.List;
 import java.util.Set;
 
 public class Bike {
-	static private int nextID = 1;
-	
-	int bikeId;
-	BikeType type;
-	int ownerId;
-	BigDecimal originalValue;
-	BikeStatus status;
-	List<Location> storeLocations;
-	List<DateRange> reservationDates;
-	LocalDate productionDate;
-	
-	
-	public Bike(BikeType type, int ownerId, BigDecimal originalValue, BikeStatus status, LocalDate productionDate) {
-		bikeId = nextID++;
-		BigDecimal depreciationRate;
-		this.type = type;
-		this.ownerId = ownerId;
-		this.originalValue = originalValue;
-		this.status = status;
-		this.productionDate = productionDate;
-		this.reservationDates = new ArrayList<DateRange>();
-		this.storeLocations = new ArrayList<Location>();
-		
-	}
-	
-    public BikeType getType() {
-    	return this.type;
-      
-    }
+  static private int nextID = 1;
+
+  private int bikeId;
+  private BikeType type;
+  private int ownerId;
+  private BigDecimal originalValue;
+  private BikeStatus status;
+  private List<Location> storeLocations;
+  private List<DateRange> reservationDates;
+  private LocalDate productionDate;
+
+
+  public Bike(BikeType type, int ownerId, BigDecimal originalValue, BikeStatus status, LocalDate productionDate) {
+    bikeId = nextID++;
+    BigDecimal depreciationRate;
+    this.type = type;
+    this.ownerId = ownerId;
+    this.originalValue = originalValue;
+    this.status = status;
+    this.productionDate = productionDate;
+    this.reservationDates = new ArrayList<>();
+    this.storeLocations = new ArrayList<>();
+
+  }
+
+  public String getType() {
+    return type.getTypeName();
+
+  }
+
+  public BigDecimal getReplacementValue() {
+    return type.getReplacementValue();
+  }
 }
