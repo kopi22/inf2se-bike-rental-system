25a19abb5daade8171a50e43d19ee612c48306cd
Linda <linda.mazanova@gmail.com>
2019-11-23 19:10:13 +0000

    Some more class edits

--- src/uk/ac/ed/bikerental/BikeProvider.java
+++ src/uk/ac/ed/bikerental/BikeProvider.java
@@ -1,5 +1,53 @@
 package uk.ac.ed.bikerental;
 
+import java.lang.ModuleLayer.Controller;
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+
 public class BikeProvider {
+	
+	String bikeProviderName;
+	Controller controller;
+	int bikeProviderId;
+	String address;
+	int phoneNumber;
+	Map<String, String> openingHours;
+	Collection<Bike> bikes;
+	Collection<Integer> partnerIDs;
+	Map<String, Double> pricingScheme;
+	BigDecimal depositRate;
+	PricingPolicy pricingPolicy;
+	DepositPolicy depositPolicy;
+	
+	public Quote getQuote(Map<Integer, Integer> bikes, DateRange dateRange) {
+		return null;
+		
+	}
+	
+	public boolean checkAvailability(Collection<Bike> bikes,DateRange dateRange ) {
+		return false;
+	}
+	
+	public void returnBike(Bike bike,DateRange dateRange) {
+		
+	}
+	
+	public boolean bookBikes (Collection<Bike> bikes, DateRange dateRange) {
+		return false;
+	}
+	
+	public void bikesReturnedToShop(Collection<Bike> bikes) {
+		
+	}
+	
+	public void bikesReturnedToPartner(Collection<Bike> bikes, Location partnerLocation) {
+		
+	}
+	
+	
+	
 
 }
--- src/uk/ac/ed/bikerental/Booking.java
+++ src/uk/ac/ed/bikerental/Booking.java
@@ -1,5 +1,20 @@
 package uk.ac.ed.bikerental;
 
+import java.math.BigDecimal;
+import java.util.Collection;
+
 public class Booking {
+	private int bikeProviderID;
+	private int orderID;
+	private Collection<Integer> orderedBikesIDs;
+	private DateRange dateRange;
+	private int returnShopID;
+	private Location deliveryAddress;
+	private Payment payment;
+	private BigDecimal deposit;
+	private DepositStatus depositStatus;
+	
+	
+	
 
 }
