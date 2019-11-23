17084d7dccf26cf8c0a0e11f4d96d87a95ab133d
Linda <linda.mazanova@gmail.com>
2019-11-23 15:46:00 +0000

    DoubleDecliningBalanceDepreciationValuationPolicy - implementation

--- src/uk/ac/ed/bikerental/DoubleDecliningBalanceDepreciationValuationPolicy.java
+++ src/uk/ac/ed/bikerental/DoubleDecliningBalanceDepreciationValuationPolicy.java
@@ -5,10 +5,28 @@ import java.time.LocalDate;
 
 public class DoubleDecliningBalanceDepreciationValuationPolicy implements ValuationPolicy {
 
+	private BigDecimal depreciationRate;
+
+	DoubleDecliningBalanceDepreciationValuationPolicy(BigDecimal depreciationRate) {
+		this.depreciationRate = depreciationRate;
+	}
+
 	@Override
-	public BigDecimal calculateValue(Bike bike, LocalDate date) {
-		// TODO Auto-generated method stub
-		return null;
+	public BigDecimal calculateValue(Bike bike, LocalDate reservartionStartDate) {
+
+		LocalDate productionDate = bike.getProductionDate();
+		if (productionDate.isAfter(reservartionStartDate)) {
+			return null;
+		}
+		BigDecimal bikeAge = BigDecimal.valueOf(new DateRange(productionDate, reservartionStartDate).toYears());
+		BigDecimal replacementValue = bike.getReplacementValue();
+
+		BigDecimal result = replacementValue.multiply(
+				(BigDecimal.valueOf(1.0).subtract(depreciationRate.multiply(BigDecimal.valueOf(2.0)))).pow(bikeAge.intValue())
+			);
+
+
+		return result;
 	}
 
 }
