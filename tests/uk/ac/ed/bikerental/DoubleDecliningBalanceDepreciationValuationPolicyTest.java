3d14685793c5dec3e490c136fe9a180ce9abf9e1
kopi22 <kpijanowski99@gmail.com>
2019-11-23 15:37:49 +0000

    Implemented Tests for DDBalane Deposit

--- tests/uk/ac/ed/bikerental/DoubleDecliningBalanceDepreciationValuationPolicyTest.java
+++ tests/uk/ac/ed/bikerental/DoubleDecliningBalanceDepreciationValuationPolicyTest.java
@@ -5,30 +5,43 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DoubleDecliningBalanceDepreciationValuationPolicyTest {
-	
-//	@Test
-//	void calculateValueBeforeBikeProduction() {
-//		LocalDate date = LocalDate.of(2010, 1, 1);
-//		Bike bike = new Bike(null,
-//				1,
-//				LocalDate.of(2018, 4, 5),
-//				BigDecimal.valueOf(0.1));
-//		ValuationPolicy valuationPolicy = new DoubleDecliningBalanceDepreciationValuationPolicy();
-//		BigDecimal value = valuationPolicy.calculateValue(bike, date);
-//
-//		assertNull(value);
-//	}
 
-//	@Test
-//	void test() {
-//		DoubleDecliningBalanceDepreciationValuationPolicy valuationPolicy = new DoubleDecliningBalanceDepreciationValuationPolicy();
-//		Bike testBike = new Bike();
-//		LocalDate testLocalDate = new
-//		
-//		BigDecimal resultBigDecimal = valuationPolicy.calculateValue(bike, date);
-//	}
+  private BikeType bikeType;
+  private ValuationPolicy valuationPolicy;
+  private Bike bike;
 
+
+  @BeforeEach
+  void setUp() {
+    try {
+      bikeType = new BikeType("BIKE");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    valuationPolicy = new DoubleDecliningBalanceDepreciationValuationPolicy(BigDecimal.valueOf(0.1));
+    bike = new Bike(bikeType,
+        1,
+        LocalDate.of(2015, 4, 5));
+  }
+
+  @Test
+  void calculateValueBeforeBikeProduction() {
+    LocalDate date = LocalDate.of(2010, 1, 1);
+    BigDecimal value = valuationPolicy.calculateValue(bike, date);
+
+    assertNull(value);
+  }
+
+  @Test
+  void test() {
+    LocalDate date = LocalDate.of(2018, 9, 1);
+    BigDecimal value = valuationPolicy.calculateValue(bike, date);
+
+    assertEquals(BigDecimal.valueOf(460.8).stripTrailingZeros(), value.stripTrailingZeros());
+  }
 }
